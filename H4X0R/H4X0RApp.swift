92eff35dad90074ee1287e6a6f821a39a1a8ce44
ponchotonic <ponchotonic@gmail.com>
2023-05-24 15:30:24 -0600

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Alfonso Castro Flores on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
